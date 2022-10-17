85b018c67328694178e8ed2fd9b93c66ec052b06
SuQiang2007 <2573040560@qq.com>
2022-10-17 15:58:49 +0800

    Update userConfig.ts

--- userConfig.ts
+++ userConfig.ts
@@ -1,11 +1,10 @@
 export const userConfig = {
   collections :[
     // "B36imhYgrNJcD7FqJxRGzxsjDpTSjciNoL9sSAMGGn6c", 
-    //"BXqCckKEidhJUpYrg4u2ocdiDKwJY3WujHvVDPTMf6nL"
+    "BXqCckKEidhJUpYrg4u2ocdiDKwJY3WujHvVDPTMf6nL"
     // "Sw5dRtqE3zL59sLdBbGkHwM8WQ7V5zBPbVW6FjPmDJC"
-    "qkuKJWMi14rdYLPBghfBRDpJASdbcLU6xZH3cpUZfng"
   ],
-  xApiKey: 'mw_RRetgcSVAXj6OpssSBj0t0PXbqsahlbnDaJ',
+  xApiKey: 'mw_testAmRKdRbBsBbIAw3CeMqS9GORmcG5BRUCU4D',
   gasFee: 4.25, // 0.001% ～ 100%
   currencyOption: 'SOL',// USDC 和 SOL
   name: 'Mirror Jump',
