540c2fa36c4e3859df9ea9f051f9c1fcb9593abe
nancyliuyang <liu_yangchina@126.com>
2022-10-21 16:28:12 +0800

    bugfix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;"
+        />
       </Head>
       <Component {...pageProps} />
     </>
